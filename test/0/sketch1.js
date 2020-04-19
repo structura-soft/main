0077d57f8edd240f7ed2302aca46a7a7be99a7cc
structura-soft <63597765+structura-soft@users.noreply.github.com>
2020-04-18 21:20:06 -0300

    Update sketch1.js

--- test/0/sketch1.js
+++ test/0/sketch1.js
@@ -46,7 +46,7 @@ constructor(x,y,radios,speed,ids)
     print(this.r);
     this.g = random(20,50);
     this.b = random(50,100);
-    fill(this.r,this.g,this.b,50%);
+    fill(this.r,this.g,this.b,0.2);
     
     circle(x, y, this.radio);
     
